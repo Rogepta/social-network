c8d15e09a1c4ec67620dcc7564a9247a26cb9cf9
Daniil Dergunov <100157798+Rogepta@users.noreply.github.com>
2023-04-21 14:53:15 +0300

    [FEATURE] Made file components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+      </div>
+      <div>ava + descr</div>
+      <div>
+        My posts
+        <div>new post</div>
+      </div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  );
+};
+
+export default Profile;
